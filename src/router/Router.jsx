2ed8fa2295606f1287776e7dd391080b57725ff3
Tangila Khatun <ktangila6@gmail.com>
2025-12-06 20:22:41 +0600

    Initialize React project and configure routing

---
+++ src/router/Router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+]);
+export default router
